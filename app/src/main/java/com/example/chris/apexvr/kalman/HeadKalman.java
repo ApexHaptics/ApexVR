394ab7ad1b81850bf71e2f3db2276ea3ac74f9ba
Chris <cv.virtue@gmail.com>
2017-03-09 09:33:09 -0500

    more kalman

--- app/src/main/java/com/example/chris/apexvr/kalman/HeadKalman.java
+++ app/src/main/java/com/example/chris/apexvr/kalman/HeadKalman.java
@@ -180,7 +180,7 @@ public class HeadKalman {
 
             pos[0] = (float) (pos[0]*0.5-0.5*headPacket.X);
             pos[1] = (float) (pos[1]*0.5+0.5*(headPacket.Y -1.8f));
-            pos[2] = (float) (pos[2]*0.5+0.5*headPacket.Z);
+            pos[2] = (float) (pos[2]*0.5+0.5*(headPacket.Z+1.5f));
 
 
             Matrix.translateM(translation,0,0,-1.2f,0);
