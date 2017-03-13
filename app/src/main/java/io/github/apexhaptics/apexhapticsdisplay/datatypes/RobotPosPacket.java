3b27cbec138998ba60abc585ef869effe13e14ba
jadentom <jadenttom@gmail.com>
2017-03-12 22:54:40 -0400

    Changed packet definitions

--- app/src/main/java/io/github/apexhaptics/apexhapticsdisplay/datatypes/RobotPosPacket.java
+++ app/src/main/java/io/github/apexhaptics/apexhapticsdisplay/datatypes/RobotPosPacket.java
@@ -6,17 +6,19 @@ import static android.R.attr.angle;
  * Created by Jaden on 2017-02-15.
  */
 
-public class EndEffectorMarkerPacket extends BluetoothDataPacket {
-    public static final String packetString = "MLoc_EEF";
-    public static final String eefString = "EEF";
+public class RobotPosPacket extends BluetoothDataPacket {
+    public static final String packetString = "MLoc_ROB";
+    public static final String robString = "ROB";
     public float X;
     public float Y;
     public float Z;
+    public float[] rotMat;
 
-    public void setEEPos(float x, float y, float z) {
+    public void setRobotPos(float x, float y, float z, float[] rotMat) {
         X=x;
         Y=y;
         Z=z;
+        this.rotMat = rotMat;
     }
 
     public String getPacketString() { return packetString; }
